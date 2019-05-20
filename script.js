6e3b8cd67dca81059576e1b1387a36dc6e6e5348
danielevanc <danielevanc2@gmail.com>
2019-05-20 20:03:21 +0700

    jawabannya

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function min(arr) {
- return Math.min(...arr)
+ return Math.min (...arr)
 }
 
 function max(arr) {
-return Math.max(...arr)
+return arr.reduce(function(x,y){
+  return (x > y) ? x : y
+})
 }
 
 function mean(arr) {
@@ -21,7 +23,7 @@ function odds(arr) {
     if (arr[i] % 2 !== 0) {
      result.push(arr[i])
     }}
-    return result
+    return result.join('-')
   
 }
 
@@ -31,7 +33,7 @@ function evens(arr) {
     if (arr[i] % 2 === 0) {
      result.push(arr[i])
     }}
-    return result
+    return result.join ('-')
   
 
 }
@@ -40,5 +42,5 @@ function numberProcessing(arr) {
   return 'Min: ' + min(arr) + ', Max: ' + max(arr) + ', Mean: ' + mean(arr) + ', Odds: ' + odds(arr) + ', Evens: ' + evens(arr)
 }
 
-console.log(numberProcessing([1, 3, 5, 1, 2, 8, 15, -5, 3]))
+console.log(numberProcessing([1, 3, 5, 1, 2, 8, 10, 0, 3]))
 // "Min: 0, Max: 10, Mean: 4, Odds: 1-3-5-1-3, Evens: 2-8-10-0
